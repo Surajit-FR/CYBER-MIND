616726ef5ca311dc881eef1ad11e8531d036d33a
Surajit-FR <miltonbaker.psoriatic@gmail.com>
2024-06-17 18:15:49 +0530

    update

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const os = require('os');
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const { ConnectToDataBase } = require('./config/database_config');
 const http = require('http');
 const socketIo = require('socket.io');
-
-const { ConnectToDataBase } = require('./config/database_config');
 const { handleConnection } = require('./services/Chat.service');
 
 const AuthRoutes = require('./routes/Auth.routes');
 const UserRoutes = require('./routes/User.routes');
 const UtilityRoutes = require('./routes/Utility.routes');
 const TransactionRoutes = require('./routes/Transaction.routes');
+
 const AdminRoutes = require('./routes/admin/Admin.routes');
 
 require('dotenv').config();
 
 // Database connection
-ConnectToDataBase();
+ConnectToDataBase()
 
 const app = express();
 
@@ -32,33 +33,51 @@ app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+// Socket.IO setup
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Health Check Route
+// Server Health check
 app.get('/health', (req, res) => {
     try {
         const networkInterfaces = os.networkInterfaces();
-        const IPv4Addresses = Object.values(networkInterfaces)
+
+        // Extract IPv4 adresses
+        const IPv4Adresses = Object.values(networkInterfaces)
             .flat()
             .filter(interfaceInfo => interfaceInfo.family === 'IPv4')
             .map(interfaceInfo => interfaceInfo.address);
 
-        const message = {
-            host: IPv4Addresses,
-            message: mongoose.connection.readyState === 1 ? 'Healthy' : 'Unhealthy',
-            status: mongoose.connection.readyState === 1,
-            time: new Date(),
-        };
-        console.log(message);
-        return res.status(200).json({ response: message });
+        if (mongoose.connection.name) {
+            const message = {
+                host: IPv4Adresses,
+                message: 'Healthy',
+                status: true,
+                time: new Date(),
+            };
+            console.log(message);
+            return res.status(200).json({ response: message });
+        } else {
+            const message = {
+                host: IPv4Adresses,
+                message: 'Unhealthy',
+                status: false,
+                time: new Date(),
+            };
+            console.log(message);
+            return res.status(501).json({ response: message });
+        }
     } catch (error) {
-        return res.status(500).json({ response: error.message });
+        return res.status(500).json({ response: error.message })
     }
 });
 
-/* API Routes */
+/* ADMIN */
+// API routes
 app.use('/api/admin', AdminRoutes);
+
+/* USER */
+// API routes
 app.use('/api/auth', AuthRoutes);
 app.use('/api', UserRoutes);
 app.use('/api', UtilityRoutes);
@@ -68,7 +87,7 @@ app.get('/api/server/check', (req, res) => {
     res.send("Hi!...I am server, Happy to see you boss...");
 });
 
-// Error Handling Middleware
+// Internal server error handeling middleware
 app.use((err, req, res, next) => {
     res.status(500).json({
         status: 500,
@@ -77,7 +96,8 @@ app.use((err, req, res, next) => {
     });
 });
 
-// 404 Middleware
+
+// Page not found middleware
 app.use((req, res, next) => {
     res.status(404).json({
         status: 404,
@@ -86,11 +106,12 @@ app.use((req, res, next) => {
 });
 
 const PORT = process.env.PORT || 4400;
-const HOST = '0.0.0.0';
+const HOST = `${process.env.HOST}:${PORT}` || `http://localhost:${PORT}`;
 
+
+// Socket server
 io.on('connection', handleConnection);
 
-// Listen on specified port and host
-server.listen(PORT, HOST, () => {
-    console.log(`Server Connected On Port ${PORT}`);
+server.listen(PORT, () => {
+    console.log(`Server Connected On Port ${HOST}`)
 });
